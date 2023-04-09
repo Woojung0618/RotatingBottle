32a981db77dc38354d70d2e5acd3ae88a4f97158
전우정 <woojung0618@gmail.com>
2023-04-09 15:28:44 +0900

    [FEAT] 병돌리기 게임 ver.1

---
+++ script.js
@@ -0,0 +1,53 @@
+const loader = new THREE.TextureLoader();
+const texture = loader.load('soju.png');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
+
+let isRotating = false;
+let rotationSpeed = 0.25;
+let rotationDuration = 4500; // 5 seconds
+let rotationStartTime = 0;
+
+function rotateBox() {
+  if (!isRotating) {
+    isRotating = true;
+    rotationStartTime = Date.now();
+    document.getElementById("win-message").style.display = "none";
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  if (isRotating) {
+    const rotationTime = Date.now() - rotationStartTime;
+    if (rotationTime >= rotationDuration) {
+      isRotating = false;
+      rotationSpeed = 0;
+      document.getElementById("win-message").style.display = "block";
+    } else {
+      rotationSpeed = (rotationDuration - rotationTime) / rotationDuration * 0.25;
+    }
+    box.rotation.z += rotationSpeed;
+  }
+
+  renderer.render(scene, camera);
+}
+
+animate();
