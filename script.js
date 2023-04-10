84202bda67d4f2f15d98bd5ddcff6cc86baf0c84
전우정 <woojung0618@gmail.com>
2023-04-10 21:48:09 +0900

    [FEAT] coffee, finger 추가

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 const loader = new THREE.TextureLoader();
-const texture = loader.load('soju.png');
+let texture = loader.load("./images/soju.png");
+
+function changeTexture(name) {
+  const loader = new THREE.TextureLoader();
+  const newTexture = loader.load(`./images/${name}.png`);
+  material.map = newTexture;
+  material.needsUpdate = true;
+  document.getElementById("win-message").style.display = "none";
+
+  // 버튼 이미지에 해당하는 문구를 표시
+  let message = "";
+  switch (name) {
+    case "soju":
+      message = "병돌리기 시~ 작!";
+      color = "#008000";
+      hoverColor = "FF0000";
+      break;
+    case "coffee":
+      message = "오늘의 커피 요정은 ~?";
+      color = "#8A4B08";
+      hoverColor = "#F7BE81";
+      break;
+    case "finger":
+      message = "술래는 바로 너!";
+      color = "#FA58F4";
+      hoverColor = "#2EFE9A";
+      break;
+    default:
+      message = "병돌리기 시~ 작!";
+      color = "#008000";
+      hoverColor = "FF0000";
+  }
+  document.querySelector("button").textContent = message;
+  document.querySelector("button").style.backgroundColor = color;
+
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+      button.style.backgroundColor = hoverColor;
+    });
+    button.addEventListener("mouseout", () => {
+      button.style.backgroundColor = color;
+    });
+  });
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,7 +58,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(0.8, 1, 1);
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
@@ -42,7 +86,9 @@ function animate() {
       rotationSpeed = 0;
       document.getElementById("win-message").style.display = "block";
     } else {
-      rotationSpeed = (rotationDuration - rotationTime) / rotationDuration * (Math.random() * 0.5 + 0.1);
+      rotationSpeed =
+        ((rotationDuration - rotationTime) / rotationDuration) *
+        (Math.random() * 0.5 + 0.1);
     }
     box.rotation.z += rotationSpeed;
   }
